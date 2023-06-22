68c13af0df66339f8ecab8735db8dbae0f4a4153
nauvalachid <nauvalachid@gmail.com>
2023-06-22 14:58:33 +0700

    cek apakah antrian penuh

--- circularQueves_117/circularQueves_117.cpp
+++ circularQueves_117/circularQueves_117.cpp
@@ -10,3 +10,15 @@ public:
 		FRONT = -1;
 		REAR = -1;
 	}
+
+	void insert() {
+		int num;
+		cout << "Enter a number:";
+		cin >> num;
+		cout << endl;
+
+		//cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
